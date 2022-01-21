2d16c90183ced24816ed532872897e01acc4164f
javtr <javtriana.z@gmail.com>
2022-01-20 22:54:00 -0500

    conect bd

--- back/app.js
+++ back/app.js
@@ -4,10 +4,10 @@ const bodyParser = require("body-parser");
 const app = express();
 const { API_VERSION } = require("./config");
 
-// Load routing 
+// Load routing
 // ...
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Configure Header HTTP
--- back/config.js
+++ back/config.js
@@ -1,6 +1,9 @@
-const API_VERSION = "v1";
+const API_VERSION = "nueva";
+const IP_SERVER = "localhost";
+const PORT_DB = 27017;
 
 module.exports = {
-	API_VERSION
+  API_VERSION,
+  IP_SERVER,
+  PORT_DB,
 };
-
