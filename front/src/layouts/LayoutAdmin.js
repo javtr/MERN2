813fea26b3b372a51b61dc3b416e94112680d64c
javtr <javtriana.z@gmail.com>
2022-01-31 22:03:39 -0500

    menu-slide

--- front/src/layouts/LayoutAdmin.js
+++ front/src/layouts/LayoutAdmin.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState }  from "react";
 import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
 import MenuTop from "../components/Admin/MenuTop"
+import MenuSider from "../components/Admin/MenuSider"
 
 import "./LayoutAdmin.scss";
 import { Content, Footer, Header } from "antd/lib/layout/layout";
 
-export default function LayoutAdmin() {
+export default function LayoutAdmin(props) {
+const [menuCollapse, setMenuCollapsed] = useState(false);
+
+
   return (
     <Layout>
+      <MenuSider/>
       <Layout className="layout-admin">
         <Header className="layout-admin__header">
           <MenuTop/>
