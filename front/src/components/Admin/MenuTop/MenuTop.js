4671b64e34afd4d01b78326293d3b12d5c8763be
javtr <javtriana.z@gmail.com>
2022-01-31 20:06:35 -0500

    menu-top

--- front/src/components/Admin/MenuTop/MenuTop.js
+++ front/src/components/Admin/MenuTop/MenuTop.js
@@ -1,6 +1,25 @@
 import React from "react";
+import logo from "../../../assets/img/png/logo-white.png";
+import { Button } from "antd";
+import { MenuUnfoldOutlined, PoweroffOutlined } from "@ant-design/icons";
+
 import "./MenuTop.scss";
 
 export default function MenuTop() {
-  return <h2>MenuTop</h2>;
+  return (
+    <div className="menu-top">
+      <div className="menu-top__left">
+        <img className="menu-top__left-logo" src={logo} alt="algo" />
+        <Button type="link" onClick={() => console.log("click")}>
+          <MenuUnfoldOutlined />
+        </Button>
+      </div>
+
+      <div className="menu-top__right">
+        <Button type="link" onClick={() => console.log("clickOut")}>
+        <PoweroffOutlined />
+        </Button>
+      </div>
+    </div>
+  );
 }
