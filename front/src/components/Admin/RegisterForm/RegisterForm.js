c68a2d8dcbbcd8e01091d2384424a064115b2e7f
javtr <javtriana.z@gmail.com>
2022-02-16 22:48:39 -0500

    emailValidation

--- front/src/components/Admin/RegisterForm/RegisterForm.js
+++ front/src/components/Admin/RegisterForm/RegisterForm.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { UserOutlined, UnlockOutlined } from "@ant-design/icons";
-import { Form, Input, Button, Checkbox, Notification } from "antd";
+import { Form, Input, Button, Checkbox, notification } from "antd";
+
+import {
+  emailValidation,
+  minLengthValidation,
+} from "../../../utils/formValidation";
+
 import "./RegisterForm.scss";
 
 export default function RegisterForm() {
@@ -8,6 +14,13 @@ export default function RegisterForm() {
     email: "",
     password: "",
     repeatPassword: "",
+    privacyPolicy: false,
+  });
+
+  const [formValid, setFormValid] = useState({
+    email: false,
+    password: false,
+    repeatPassword: false,
     privacyPolicy: false,
   });
 
@@ -25,9 +38,64 @@ export default function RegisterForm() {
     }
   };
 
-  const register = e => {
-    console.log(inputs);
+  const inputValidation = (e) => {
+    const { type, name } = e.target;
+
+    if (type === "email") {
+      setFormValid({ ...formValid, [name]: emailValidation(e.target) });
+    }
+    if (type === "password") {
+      setFormValid({ ...formValid, [name]: minLengthValidation(e.target, 6) });
+    }
+    if (type === "checkbox") {
+      setFormValid({ ...formValid, [name]: e.target.checked });
+    }
   };
+
+  const register = async (e) => {
+    // e.preventDefault();
+
+    const emailVal = inputs.email;
+    const passwordVal = inputs.password;
+    const repeatPasswordVal = inputs.repeatPassword;
+    const privacyPolicyVal = inputs.privacyPolicy;
+
+    if (!emailVal || !passwordVal || !repeatPasswordVal || !privacyPolicyVal) {
+      notification["error"]({
+        message: "Todos los campos son obligatorios",
+      });
+    } else {
+      if (passwordVal !== repeatPasswordVal) {
+        notification["error"]({
+          message: "Las contraseñas tienen que ser iguales.",
+        });
+      } else {
+
+        console.log(inputs);
+
+        // const result = await signUpApi(inputs);
+        // if (!result.ok) {
+        //   notification["error"]({
+        //     message: result.message,
+        //   });
+        // } else {
+        //   notification["success"]({
+        //     message: result.message,
+        //   });
+        //   resetForm();
+        // }
+      }
+    }
+  };
+
+  // const resetForm = () => {
+  //   const inputs = document.getElementsByTagName("input");
+
+  //   for (let i = 0; i < inputs.length; i++) {
+  //     inputs[i].classList.remove("success");
+  //     inputs[i].classList.remove("error");
+  //   }
+  // };
 
   return (
     <Form className="register-form" onFinish={register} onChange={changeForm}>
@@ -38,6 +106,7 @@ export default function RegisterForm() {
           name="email"
           placeholder="Correo electronico"
           className="register-form__input"
+          onChange={inputValidation}
           value={inputs.email}
         />
       </Form.Item>
@@ -49,8 +118,8 @@ export default function RegisterForm() {
           name="password"
           placeholder="Contraseña"
           className="register-form__input"
+          onChange={inputValidation}
           value={inputs.password}
-          
         />
       </Form.Item>
 
@@ -61,18 +130,27 @@ export default function RegisterForm() {
           name="repeatPassword"
           placeholder="Repetir contraseña"
           className="register-form__input"
+          onChange={inputValidation}
           value={inputs.repeatPassword}
         />
       </Form.Item>
 
       <Form.Item>
-        <Checkbox name="privacyPolicy" checked={inputs.privacyPolicy}>
+        <Checkbox
+          name="privacyPolicy"
+          onChange={inputValidation}
+          checked={inputs.privacyPolicy}
+        >
           He leido y acepto la politica de privacidad
         </Checkbox>
       </Form.Item>
 
       <Form.Item>
-        <Button type="primary" htmlType="submit" className="register-form__button">
+        <Button
+          type="primary"
+          htmlType="submit"
+          className="register-form__button"
+        >
           crear cuenta
         </Button>
       </Form.Item>
